909371f0dab443a5914816952271d9399998bf49
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-01-08 14:31:21 +0000

    deployment to heroku

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
 """
 import os standard python library, flask class & render_template function
 """
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 """
 storing it in a variable called app.
 first argument of the Flask class is the application's module - our package
 as its a single module, we use __name__ which is a built in python variable
-decorator "/"browses the root directory which triggers the function underneath
+decorator "/"browses the root directory & triggers the function underneath
 """
 
 
@@ -20,25 +25,42 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET","POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        flash(
+            "Thanks {}, we have received your message!".format(
+                request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
-    )
+        debug=True)
 
 
 """
